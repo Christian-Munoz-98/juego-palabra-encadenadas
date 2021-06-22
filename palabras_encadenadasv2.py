20d7255d2ed30f174c94e263270e172687d9d391
Christian Muñoz <chgeovany.15@gmail.com>
2021-06-22 17:59:46 -0500

    Antes de cagarla

--- palabras_encadenadasv2.py
+++ palabras_encadenadasv2.py
@@ -4,15 +4,13 @@ def registro():
     
     jugadores = {}
 
-    jugador_1 = input('Ingrese el nombre del primer jugador: ')
+    jugador_1 = input('\nIngrese el nombre del primer jugador: ')
     jugadores.setdefault('Jugador 1',jugador_1)
-    jugadores.setdefault('Puntuacion 1',0)
 
-    jugador_2 = input('Ingrese el nombre del segundo jugador: ')
+    jugador_2 = input('\nIngrese el nombre del segundo jugador: ')
     jugadores.setdefault('Jugador 2',jugador_2)
-    jugadores.setdefault('Puntuacion 2',0)
 
-    print('¡Registro exitoso!')
+    print('\n'+'¡Registro exitoso!'.center(30))
     return jugadores
 
 def juego(jugadores):
@@ -88,11 +86,11 @@ def juego(jugadores):
                 num_jugador = 1
             
         if puntuacion_1 > puntuacion_2:
-            print('\nEl ganador es:\n\n' + (jugadores['Jugador 1'].upper()).center(30) + '\n\n')
+            print('\n'+'EL GANADOR ES'.center(30,'=') +'\n'+(jugadores['Jugador 1'].upper()).center(30) + '\n'+'='*30)
         elif puntuacion_2 > puntuacion_1:
-            print('\n' + 'El ganador es:'.center(30) +'\n\n'+ (jugadores['Jugador 2'].upper()).center(30) + '\n\n')
+            print('\n'+'EL GANADOR ES'.center(30,'=') +'\n' + (jugadores['Jugador 2'].upper()).center(30) + '\n'+'='*30)
         else:
-            print('\n LA PARTIDA TERMINO EN EMPATE\n\n')
+            print('\n'+'='*28+'\nLA PARTIDA TERMINÓ EN EMPATE\n'+'='*28+'\n')
     
         print('Puntuaciones:\n'+ jugadores['Jugador 1'] + ' ' + '='*10 + ' '+ str(puntuacion_1) + '\n' + jugadores['Jugador 2'] + ' ' + '='*10 + ' ' + str(puntuacion_2))
 
@@ -107,54 +105,61 @@ def juego(jugadores):
 
 def consulta():
     global registro_puntaciones
+    lista_ordenada = []
+    
+    if len(registro_puntaciones) == 0 :
+        print('\n'+'¡ERROR!'.center(51))
+        print('No hay puntuaciones registradas en la base de datos')
 
-    while True:
-        nombre = input("Ingrese el nombre del jugador que desea consultrar: ")
+    else:
+        while True:
+            nombre = input("\nIngrese el nombre del jugador que desea consultrar: ")
 
-        if nombre in registro_puntaciones.keys():
-            for partida,puntuacion in enumerate(registro_puntaciones[nombre]):
-                print(f'Partida {partida+1} obtuvo {puntuacion} puntos')
-            
-            for jugador,puntuaciones in registro_puntaciones.items():
-                if jugador == nombre:
-                    continue
-                puntuacion_global = 0
-                for partida in puntuaciones:
-                    puntuacion_global += partida
-                print(f'{jugador} Puntuación global: {puntuacion_global}')
-                
-            break
-        print("¡Eror!. Ingrese un nombre que se encuentre disponible en la base de datos.")
+            if nombre in registro_puntaciones.keys():
+                print('\n'+'PARTIDAS DEL JUGADOR'.center(29,'='))
+                for partida,puntuacion in enumerate(registro_puntaciones[nombre]):
+                    print(f'Partida {partida+1}: obtuvo {puntuacion} puntos')
+                print('='*30)
+                print('PUNTUACIONES GLOBALES'.center(29,'='))
+                for jugador,puntuaciones in registro_puntaciones.items():
+                    if jugador == nombre:
+                        continue
+                    puntuacion_global = 0
+                    for partida in puntuaciones:
+                        puntuacion_global += partida
+                    print(f'{jugador} ---------- {puntuacion_global}')
+                    print('='*30)
+                break
+            print("\n¡Eror!. Ingrese un nombre que se encuentre disponible en la base de datos.")
 
-def main():
+def menu():
     global jugadores
-    global registro_puntaciones
-    print('ELIJA UNA OPCIÓN'.center(30,'-')+'\n'+'1.Registrar Jugadores'.ljust(30,'-')+'\n'+'2.Verificar puntuaciones'.ljust(30,'-')+'\n'+'3.Jugar'.ljust(30,'-')+'\n'+'4.salir'.ljust(30,'-'))
+    print('\n'+'ELIJA UNA OPCIÓN'.center(30,'-')+'\n'+'1.Registrar Jugadores'.ljust(30,'-')+'\n'+'2.Verificar puntuaciones'.ljust(30,'-')+'\n'+'3.Jugar'.ljust(30,'-')+'\n'+'4.salir'.ljust(30,'-'))
     opcion = int(input('\n'+ 'Selección: '))
 
     if opcion == 1:
         jugadores = registro()
-        main()
+        menu()
     elif opcion == 2:
         consulta()
-        main()
+        menu()
     elif opcion == 3:
         juego(jugadores)
-        main()
+        menu()
     elif opcion == 4:
         print('\n'+'Saliendo del programa...'.center(30))
         print('\n'+'Que tenga buen dia!'.center(30))
     else:
         print('\nError! Elija una opción válida...\n')
-        main()
+        menu()
 
 if __name__ == '__main__':
     registro_puntaciones = {}
     jugadores = {}
-    main()
+    menu()
 
-#Ver si puedo quitar el global statement de la funcion main()
 #Ordenar las puntuaciones globales de mayor a menor
 #Omitir el registro de las palabras que se ingresron dos veces consecutivas
 #Restar dos puntos cuando el jugador ingresa una palabra repetida consecutivmente
-#Estilizar los prints
+#Colorear prints
+#Documentar el funcionamiento
